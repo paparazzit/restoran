207e36228adb032f0f890a2cad13bc77431261d8
paparazzit <paparazzit@fgmail.com>
2021-01-28 09:25:06 +0100

    responsive sa CTA

---
+++ butterinit.js
@@ -0,0 +1,1 @@
+butter.init({ cancelOnTouch: true });
--- main.js
+++ main.js
@@ -25,7 +25,26 @@ const navSlide = () => {
 };
 navSlide();
 
-$(".owl-carousel").owlCarousel({
+$(".slider-one").owlCarousel({
+	loop: true,
+	nav: true,
+	dots: true,
+	margin: 30,
+
+	responsive: {
+		0: {
+			items: 1,
+		},
+		1000: {
+			items: 2,
+		},
+		1300: {
+			items: 3,
+		},
+	},
+});
+
+$(".slider-two").owlCarousel({
 	loop: true,
 	nav: true,
 	dots: true,
@@ -36,19 +55,61 @@ $(".owl-carousel").owlCarousel({
 			items: 1,
 		},
 		600: {
-			items: 2,
+			items: 1,
 		},
 		1000: {
-			items: 3,
+			items: 1,
 		},
 	},
 });
 
-$(window).scroll(function () {
-	if ($("#blog1").visible()) {
-		$("body").addClass("newBG");
-		$("#blog1").addClass("remove");
-	} else {
-		$("body").removeClass("newBG");
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+	var currentScrollPos = window.pageYOffset;
+	var nav = document.querySelector("nav");
+	var logo = document.querySelector(".logo");
+	var img = document.querySelector(".img");
+
+	var target = document.querySelectorAll(".scroll");
+	// console.log(`trenutna pozicija: ${currentScrollPos}`);
+	var index = 0,
+		length = target.length;
+	for (index; index < length; index++) {
+		var pos = window.pageYOffset * target[index].dataset.rate;
+		var scale = (window.pageYOffset * target[index].dataset.scale) / 450;
+		if (target[index].dataset.direction === "vertical") {
+			target[index].style.transform = "translate3d(0px," + pos + "px, 0px)";
+		} else {
+			var posX = window.pageYOffset * target[index].dataset.ratex;
+			var posY = window.pageYOffset * target[index].dataset.ratey;
+			var scale = (window.pageYOffset * target[index].dataset.scale) / 450;
+
+			target[index].style.transform =
+				"translate3d(" + posX + "px, " + posY + "px, 0px )";
+
+			target[index].style.transform = `scale(${scale})`;
+		}
 	}
+
+	if (prevScrollpos < currentScrollPos) {
+		nav.classList.add("gone");
+		logo.classList.add("gone");
+	} else {
+		nav.classList.remove("gone");
+		logo.classList.remove("gone");
+	}
+	prevScrollpos = currentScrollPos;
+	if (currentScrollPos > 400) {
+		img.classList.add("gone");
+	} else {
+		img.classList.remove("gone");
+	}
+};
+
+var a = 0;
+
+$(document).ready(function () {
+	$(".bcta").click(function () {
+		$(".delivery").toggleClass("animate");
+	});
 });
